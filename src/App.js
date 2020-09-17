12c7d3bd56f1648a0b3d6dfc6383442e2fa93ad7
Shiwei He <irene@Shiweis-MacBook-Air.local>
2020-09-17 18:45:53 -0400

    headline component with test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './component/Header/index';
+import Headline from './component/Headline/index';
 import './app.scss';
 function App() {
   return (
     <div className='App'>
       <Header />
+      <section className='main'>
+        <Headline header='Posts' desc='Click the button to render posts' />
+      </section>
     </div>
   );
 }
