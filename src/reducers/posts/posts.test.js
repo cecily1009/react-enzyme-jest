7aadadcad3f6b04f9d670bbd1b5ab9dfb2a44945
Shiwei He <irene@Shiweis-MacBook-Air.local>
2020-09-17 20:21:46 -0400

    reducer test for posts

---
+++ src/reducers/posts/posts.test.js
@@ -0,0 +1,21 @@
+import { GET_POSTS } from './../../actions/types';
+import postsReducer from './reducer';
+describe('Posts reducer', () => {
+  it('Should return default state', () => {
+    const newState = postsReducer(undefined, {});
+    expect(newState).toEqual([]);
+  });
+
+  it('Should return new State if receiving type', () => {
+    const posts = [
+      { title: 'Test 1' },
+      { title: 'Test 2' },
+      { title: 'Test 3' },
+    ];
+    const newState = postsReducer(undefined, {
+      type: GET_POSTS,
+      payload: posts,
+    });
+    expect(newState).toEqual(posts);
+  });
+});
